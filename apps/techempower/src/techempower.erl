f50fef5e27e1f447f5a28d42caf58930368596d3
chan <mihawk@users.noreply.github.com>
2016-01-16 11:05:51 +0800

    Update techempower.erl

--- apps/techempower/src/techempower.erl
+++ apps/techempower/src/techempower.erl
@@ -7,8 +7,8 @@ main(A)    -> mad_repl:sh(A).
 start(_,_) -> supervisor:start_link({local,techempower}, techempower,[]).
 stop(_)    -> ok.
 init([])   -> naga:start([techempower]),
-			  random:seed(os:timestamp()),
-			  kvs:join(),  
+              random:seed(os:timestamp()),
+              kvs:join(),  
               sup().
 
 sup()      -> { ok, { { one_for_one, 5, 100 }, [] } }.
